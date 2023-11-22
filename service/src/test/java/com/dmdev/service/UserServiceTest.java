d307e5bfbfe871f27d1e9bd791a5578fb5959741
Ivan Khizhkin <ivan.khizhkin@gmail.com>
2023-11-22 14:56:21 +0300

    to do

---
+++ service/src/test/java/com/dmdev/service/UserServiceTest.java
@@ -0,0 +1,19 @@
+package com.dmdev.service;
+
+import com.dmdev.dao.UserDao;
+import com.dmdev.model.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    private final UserService userService = new UserService();
+    @Test
+    void getAll() {
+        List<User> actual = userService.getAll();
+        assertEquals(4, actual.size());
+    }
+}
